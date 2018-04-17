d5aaeb67e3a4ce57b25b7dc0cdf77fbc23756af1
Anton Logvinov <kotelliada@gmail.com>
2018-04-17 20:38:28 +0300

    feat: repository layer
    Add PhotoRepository class which fetch data from network.

---
+++ app/src/main/java/io/github/kotelliada/flickrlient/repository/PhotoRepository.java
@@ -0,0 +1,53 @@
+package io.github.kotelliada.flickrlient.repository;
+
+import android.arch.lifecycle.MutableLiveData;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import java.util.List;
+
+import io.github.kotelliada.flickrlient.model.Photo;
+import io.github.kotelliada.flickrlient.network.PhotoService;
+import io.github.kotelliada.flickrlient.network.ServiceBuilder;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class PhotoRepository {
+    private static final String TAG = PhotoRepository.class.getSimpleName();
+    private static PhotoRepository instance;
+    private MutableLiveData<List<Photo>> photoList = new MutableLiveData<>();
+    private PhotoService photoService;
+
+    private PhotoRepository(PhotoService photoService) {
+        this.photoService = photoService;
+    }
+
+    public static PhotoRepository getInstance(PhotoService photoService) {
+        if (instance == null)
+            instance = new PhotoRepository(photoService);
+        return instance;
+    }
+
+    public MutableLiveData<List<Photo>> getPhotoList() {
+        return photoList;
+    }
+
+    public void getRandomPhotosFromService() {
+        photoService.getRandomPhotos(ServiceBuilder.API_KEY)
+                .enqueue(new Callback<List<Photo>>() {
+                    @Override
+                    public void onResponse(@NonNull Call<List<Photo>> call, @NonNull Response<List<Photo>> response) {
+                        if (response.isSuccessful())
+                            photoList.setValue(response.body());
+                        else
+                            Log.e(TAG, "PhotoRepository.getRandomPhotosFromService(): " + response.message());
+                    }
+
+                    @Override
+                    public void onFailure(@NonNull Call<List<Photo>> call, @NonNull Throwable t) {
+                        Log.e(TAG, t.getMessage());
+                    }
+                });
+    }
+}
